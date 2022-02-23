5df4b3946822a2903e42fe601304abd90843224f
roma2121 <82339399+roma2121@users.noreply.github.com>
2022-02-23 18:17:29 +0300

    Add files via upload

---
+++ Example_1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+
+int getValue1()
+{
+	while (true)
+	{
+		cout << "Введите целое знаковое число: ";
+		register int a;
+		cin >> a;
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Число введено неверно!\n";
+		}
+		else
+		{
+			cin.ignore(32767, '\n');
+			return a;
+		}
+	}
+}
+
+int getValue2()
+{
+	while (true)
+	{
+		cout << "Введите целое число, обозначающее степень 2: ";
+		register int b;
+		cin >> b;
+		if (b < 0)
+		{
+			cout << "Число введено неверно!\n";
+		}
+		else
+		{
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(32767, '\n');
+				cout << "Число введено неверно!\n";
+			}
+			else
+			{
+				cin.ignore(32767, '\n');
+				return b;
+			}
+		}
+
+	}
+}
+
+int division(int number, int degree)
+{
+	if (number < 0)
+	{
+		number = -number;
+		return -(number >> degree);
+	}
+	else
+	{
+		return number >> degree;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+	register int number = getValue1();
+	register int degree = getValue2();
+
+	cout << "Результат вычислений: " << division(number, degree) << endl;
+
+}
